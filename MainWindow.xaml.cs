fef9c133cf048ceb4d766985fc35560bf3202a95
Рустам <xannanovr@gmail.com>
2025-12-12 16:30:06 +0400

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Explorer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_Object_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (TextBox_Object.Text != "")
+                Button_Add.IsEnabled = true;
+            else Button_Add.IsEnabled = false;
+        }
+
+        private void Button_Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
